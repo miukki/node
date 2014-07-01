7abf02aaffe311c91f068a3ed6cef1b1a0962c5b
miukki macbook air <miukki@gmail.com>
2014-07-02 00:26:07 +0800

    add rebase fn

--- currencies.js
+++ currencies.js
@@ -2,25 +2,57 @@ var http = require('http');
 var fs = require('fs');
 
 module.exports = {
-  'http': function(url, cb) {
+  'http': function(url) {
     http.get(url, function(res) {
       res.setEncoding('utf8');
-      res.on('data', function(data){
-        if (cb) {
-          cb(data);
+      var data = ''; //not null, not object, but string
+
+      res.on('data', function(chunk){
+        data += chunk;
+        console.log('data.length', data.length);
+
+        if (res.headers['content-length'] == data.length) {
+
+          fs.writeFileSync('cur.json', data);
+          console.log('done, upload!')
+
         };
+
       });
+
+
+      res.on('end', function() {
+
+        //
+      });
+
       res.on('error', function(e){
         console.log('problem with request: ' + e.message);
       });
     });
   },
+
   'getAll': function(cb){
-    fs.readFile('cur.json', function(err, data) {
+    fs.readFile('cur.json', {'encoding': 'utf8'}, function(err, data) {
       if (err) throw err;
       if (cb)  {
         cb(JSON.parse(data));
       };
     });
+  },
+
+ 'getAllSync': function() {
+    return fs.readFileSync('cur.json', {'encoding': 'utf8'});
+  },
+
+  'rebase': function(cur) {
+    var data = JSON.parse(this.getAllSync())[0];
+    var rebase = []
+    if (!data[cur]) {
+      return;
+    } else {
+
+    }
   }
+
 }
--- myprogram.js
+++ myprogram.js
@@ -2,23 +2,40 @@
 var fs = require('fs');
 var path =   require('path');
 
-var arr = require('./argparser.js')(process.argv);
+var args = require('./argparser.js')(process.argv);
 
-console.log('arr', arr);
+console.log('args', args);
 
-require('./currencies.js').http('http://www.getexchangerates.com/api/latest.json', function(data){
-  fs.writeFile('cur.json', data, {}, function() {
-    console.log('done upload!')
+if (args.indexOf('update') > -1) {
+
+  require('./currencies.js').http('http://www.getexchangerates.com/api/latest.json');
+
+};
+
+
+
+var arg  = args.filter(function(item){ return /^get/.test(item); });
+if(arg[0]) {
+  var params = arg[0].split(':')[1];
+  var cur;
+  require('./currencies.js').getAll(function(data){
+
+    if (!params) {
+      console.log(data);
+      return;
+    };
+
+    console.log('params', params.split('/')[0], params);
+
+    if (params.split('/')[0] != 'usd') {
+      cur = require('./currencies.js').rebase(params.split('/')[0]);
+    };
+
   });
-});
 
-/*
 
-require('./currencies.js').getAll(function(data){
-  console.log(data)
-});
+};
 
-*/
 
 
 //https://www.npmjs.org/package/simply-deferred  invoke
